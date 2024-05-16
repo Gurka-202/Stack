0bad340f4f24997456820e153ac6817ef142f686
Magda <93428783+Gurka-202@users.noreply.github.com>
2024-05-16 14:28:39 +0300

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,13 +1,13 @@
 #include "stack.h"
 #include <iostream>
 
-// Конструктор за замовчуванням
+
 Stack::Stack() {}
 
-// Конструктор копіювання
+
 Stack::Stack(const Stack& other) : stack(other.stack) {}
 
-// Оператор присвоєння
+
 Stack& Stack::operator=(const Stack& other) {
     if (this != &other) {
         stack = other.stack;
@@ -15,12 +15,12 @@ Stack& Stack::operator=(const Stack& other) {
     return *this;
 }
 
-// Додавання елемента в вершину стека
+
 void Stack::push(int elem) {
     stack.push_back(elem);
 }
 
-// Видалення елемента з вершини стека і повернення його значення
+
 int Stack::pop() {
     if (isEmpty()) {
         std::cerr << "Error. Stack is empty" << std::endl;
@@ -31,12 +31,12 @@ int Stack::pop() {
     return topElem;
 }
 
-// Перевірка, чи порожній стек
+
 bool Stack::isEmpty() const {
     return stack.empty();
 }
 
-// Виштовхування кількох елементів
+
 void Stack::multiPop(int N) {
     for (int i = 0; i < N; ++i) {
         if (!isEmpty()) {
@@ -49,13 +49,13 @@ void Stack::multiPop(int N) {
     }
 }
 
-// Метод для показу всіх елементів, що знаходяться в стеці
+
 void Stack::show() const {
     if (isEmpty()) {
         std::cout << "Stack is empty" << std::endl;
         return;
     }
-    //std::cout << "Show elements: ";
+   
     for (size_t i = 0; i < stack.size(); ++i) {
         std::cout << stack[i];
         if (i < stack.size() - 1) {
